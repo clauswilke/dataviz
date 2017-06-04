f8a792e4f3e70757ab074a9579fa4acc91b20a5d
Claus Wilke <wilke@austin.utexas.edu>
2017-06-04 16:55:19 -0500

    Good first chapter draft on small axis labels, plus start of chapter on overlapping points.

--- _common.R
+++ _common.R
@@ -3,7 +3,7 @@ options(digits = 3)
 
 knitr::opts_chunk$set(
   echo = FALSE,
-  cache = TRUE,
+  cache = FALSE,
   fig.align = 'center',
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
@@ -13,3 +13,15 @@ knitr::opts_chunk$set(
 options(dplyr.print_min = 6, dplyr.print_max = 6)
 
 library(cowplot)
+
+# functions to stamp figures as good, bad, or ugly
+stamp <- function(p, color, alpha, label)
+{
+  ggdraw(p) +
+    draw_text(label, x=.99, y=1, vjust=-.6, hjust=1, size=16, color=color, alpha=alpha, angle=90) +
+    draw_line(c(.99, .99), c(0, 1), size=1.4, color=color, alpha=alpha)
+}
+
+stamp_good <- function(p) stamp(p, "olivedrab4", .7, "good")
+stamp_bad <- function(p) stamp(p, "firebrick", .7, "bad")
+stamp_ugly <- function(p) stamp(p, "orange3", .7, "ugly")
