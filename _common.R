59d4bcd29231d63792a733d6976104a636bc48ee
Claus Wilke <wilke@austin.utexas.edu>
2017-06-03 20:46:09 -0500

    new text, some reorg

---
+++ _common.R
@@ -0,0 +1,16 @@
+set.seed(7654)
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  echo = FALSE,
+  cache = TRUE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+
+library(cowplot)
