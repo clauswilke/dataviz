e087f59839f4ab6b01748bf7100efddac79672f8
Claus Wilke <wilke@austin.utexas.edu>
2018-03-14 18:12:32 -0500

    Rework figures so they all have exactly the same width. Chapters 1-5 are done.

--- _common.R
+++ _common.R
@@ -7,9 +7,11 @@ knitr::opts_chunk$set(
   warning = FALSE,
   cache = FALSE,
   fig.align = 'center',
-  fig.width = 6,
-  fig.asp = 0.618,  # 1 / phi
-  fig.show = "hold"
+  fig.width = 8.5, # fig.width = 6,
+  fig.height = 0.618 * 6,
+  #fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold",
+  dpi = 600
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
