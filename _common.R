68ba29c19be681b141d4f993bceec8e561885f88
Claus Wilke <wilke@austin.utexas.edu>
2018-03-16 02:13:35 -0500

    Revert "Rework figures so they all have exactly the same width. Chapters 1-5 are done."
    This reverts commit e087f59839f4ab6b01748bf7100efddac79672f8.

--- _common.R
+++ _common.R
@@ -7,11 +7,9 @@ knitr::opts_chunk$set(
   warning = FALSE,
   cache = FALSE,
   fig.align = 'center',
-  fig.width = 8.5, # fig.width = 6,
-  fig.height = 0.618 * 6,
-  #fig.asp = 0.618,  # 1 / phi
-  fig.show = "hold",
-  dpi = 600
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
