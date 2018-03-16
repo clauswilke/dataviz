455aa6955319b878e6a403ed685dcab3f5282e56
Claus Wilke <wilke@austin.utexas.edu>
2018-03-16 01:54:13 -0500

    figure padding

---
+++ _common2.R
@@ -0,0 +1,18 @@
+set.seed(7654)
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  echo = FALSE,
+  message = FALSE,
+  warning = FALSE,
+  cache = FALSE,
+  fig.align = 'center',
+  fig.width = 5,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold",
+  dpi = 300
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+
+library(dviz.supp)
