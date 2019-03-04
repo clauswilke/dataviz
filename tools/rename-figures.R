fd09dc7c76843d0827dfe4ece8666b323e2a798d
Claus Wilke <wilke@austin.utexas.edu>
2019-03-04 10:04:59 -0600

    final figures renamed

---
+++ tools/rename-figures.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(glue)
+library(here)
+
+
+fnames <- read_csv(here("tools", "figurenames.csv"))
+
+copy_fig <- function(chnum, fignum, dir, filename, ...) {
+  src <- here("_book_production", dir, filename)
+  fignum_combined <- paste0(str_pad(chnum, 2, pad = "0"), str_pad(fignum, 2, pad = "0"))
+  dest <- here("_book_production/figures/all-final", glue("fodv_{fignum_combined}.png"))
+
+  cat(glue("Copying {src} to {dest}"), "\n")
+  file.copy(src, dest)
+  dest
+}
+
+invisible(pmap(fnames, copy_fig))
